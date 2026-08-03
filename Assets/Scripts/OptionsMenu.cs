66d039ffa339dbb55c4b58609725da9fb5918b05
Kobanchick1886 <firkin2007@gmail.com>
2026-08-03 17:35:48 +0300

    speed sliders and .exe work now

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -14,19 +14,32 @@ public class OptionsMenu : MonoBehaviour
 
     void Start()
     {
-        // 1. При открытии меню подгружаем сохраненные данные 
+        // 1. ПРИНУДИТЕЛЬНО задаем лимиты для ползунков скорости
+        if (playerSpeedSlider != null)
+        {
+            playerSpeedSlider.minValue = 50f;
+            playerSpeedSlider.maxValue = 250f;
+        }
+
+        if (enemySpeedSlider != null)
+        {
+            enemySpeedSlider.minValue = 3f;
+            enemySpeedSlider.maxValue = 15f;
+        }
+
+        // 2. Подгружаем сохраненные данные (если их нет, ставим дефолты)
         if (musicSlider != null) musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
         if (soundsSlider != null) soundsSlider.value = PlayerPrefs.GetFloat("SoundsVolume", 1f);
-        if (playerSpeedSlider != null) playerSpeedSlider.value = PlayerPrefs.GetFloat("PlayerSpeed", 5f);
-        if (enemySpeedSlider != null) enemySpeedSlider.value = PlayerPrefs.GetFloat("EnemySpeed", 3f);
+        if (playerSpeedSlider != null) playerSpeedSlider.value = PlayerPrefs.GetFloat("Speed_Player", 120f);
+        if (enemySpeedSlider != null) enemySpeedSlider.value = PlayerPrefs.GetFloat("Speed_Enemy", 8f);
 
-        // 2. Подписываем ползунки на автоматическое сохранение при любом их движении
+        // 3. Подписываем ползунки на автоматическое сохранение при любом их движении
         if (musicSlider != null) musicSlider.onValueChanged.AddListener(SaveMusic);
         if (soundsSlider != null) soundsSlider.onValueChanged.AddListener(SaveSounds);
         if (playerSpeedSlider != null) playerSpeedSlider.onValueChanged.AddListener(SavePlayerSpeed);
         if (enemySpeedSlider != null) enemySpeedSlider.onValueChanged.AddListener(SaveEnemySpeed);
 
-        // 3. Подвязываем кнопку выхода в главное меню
+        // 4. Подвязываем кнопку выхода в главное меню
         if (backButton != null) backButton.onClick.AddListener(CloseOptions);
     }
 
@@ -53,13 +66,13 @@ public class OptionsMenu : MonoBehaviour
 
     private void SavePlayerSpeed(float value)
     {
-        PlayerPrefs.SetFloat("PlayerSpeed", value);
+        PlayerPrefs.SetFloat("Speed_Player", value);
         PlayerPrefs.Save();
     }
 
     private void SaveEnemySpeed(float value)
     {
-        PlayerPrefs.SetFloat("EnemySpeed", value);
+        PlayerPrefs.SetFloat("Speed_Enemy", value);
         PlayerPrefs.Save();
     }
 }
